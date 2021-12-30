e64dc2b59920ec9b46a34fea71a9c0099a07714a
cheolwon1994 <chlcjfahs@naver.com>
2021-12-30 15:57:14 +0900

    Configuration의 용도와 Spring이 Singleton을 지원하는 방법

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,21 +15,48 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     /**
+     * @Configuration에 대한 설명
+     * 상황 예시
+     * 1. @Bean memberService -> new MemoryMemberRepository()
+     * 2. @Bean orderService -> new MemoryMemberRepository()
+     * -> new 호출 2번으로 인해 Sinlgeton이 깨지는것 아닌가?
+     *
+     * 호출 순서는 다를 수 있다. 단, Singleton이라면 memberRepository가 3번 호출되야 한다(TestCode에서)
+     * [예상]
+     * call AppConfig.memberService
+     * call AppConfig.memberRepository
+     * call AppConfig.memberRepository
+     * call AppConfig.orderService
+     * call AppConfig.memberRepository
+     *
+     * [실제]
+     * call AppConfig.memberService
+     * call AppConfig.memberRepository
+     * call AppConfig.orderService
+     *
+     * 왜?
+     * @Configuration or @Autowired를 사용할 때 SpringContainer의 경우, memberRepository를 상속하는 memberRepository@CGLIB~를 Bean으로 등록하여 싱글톤이 보장되도록 한다
+     */
+
+    /**
      * 빈 이름은 메서드 이름을 사용
      * @Bean(name="memberSer")과 같이 빈 이름 직접 설정 가능
      */
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
