8ce08844f349a502889b80ef8b744f39001740e5
cheolwon1994 <chlcjfahs@naver.com>
2021-12-31 15:32:28 +0900

    @Component 사용방법과 CompenentScan 실행 순서 설명 및 예시. 그리고 따로 설정하지 않았던 의존관계 주입을 위해 @Autowired 사용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired      //자동 의존관계 주입 위해 사용 -> 자동적으로 ac.getBean(MemberRepository.class)가 주입(타입을 통해 검색)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
