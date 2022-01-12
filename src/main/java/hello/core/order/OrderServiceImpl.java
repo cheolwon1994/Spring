1ca4a7c61a1911c37ce9c95e17c72d6d99853f70
cheolwon1994 <chlcjfahs@naver.com>
2022-01-12 16:51:11 +0900

    Annotation 직접 만드는 방법 조회한 빈은 List, Map에 담긴다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -35,8 +36,7 @@ public class OrderServiceImpl implements OrderService{
      * 중복된 타입의 빈이 있을 때, 자동주입시 @Primary가 붙은게 우선순위가 높다
      *
      * Question) @Qualifier와 @Primary가 동시에 쓰인다면 어떤것이 우선순위를 가질까?
-     * Answer) 자동보단 수동이 우선권을 가지므로, @Qualifier가 우선순위가 높다. 단, @Qualifier의 단점으로는 모든 코드에 해당 어노테이션을 붙여주어야 한다
-     *
+     * Answer) 자동보단 수동이 우선권을 가지므로, @Qualifier가 우선순위가 높다. 단, @Qualifier의 단점으로는 모든 코드에 해당 어노테이션을 붙여주어야 한다징     *
      */
     /*3. 필드 주입 @Autowired 직접 선언
     / 단점: 1. 보기엔 편리해보이지만 외부에서 접근이 불가능하다. MemberRepository와 DiscountPolicy를 세팅해줄 set 함수가 추가로 필요하다. 즉, DI 프레임워크가 없으면 아무것도 할 수 없다
@@ -79,7 +79,7 @@ public class OrderServiceImpl implements OrderService{
     //생성자가 1개 일때는 자동적으로 @Autowired가 설정된다
     만약 타입이 같을 때 필드명, 파라미터 이름에 따라 매칭한다
     */
-    public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
